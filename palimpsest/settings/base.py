a6b26d68d83e0d0b1b5228d7468d286fda9b5626
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-12-24 13:11:56 -0700

    restructure project

--- palimpsest/settings/base.py
+++ palimpsest/settings/base.py
@@ -74,10 +74,7 @@ WSGI_APPLICATION = 'palimpsest.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # postgres/psycopg2 configured in local_settings.py
 }
 
 
